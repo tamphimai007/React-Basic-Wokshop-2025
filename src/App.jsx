97ef04c146fac18e1033f86c562ee0d2b9de0d89
tamisan <phongphat.jap@gmail.com>
2025-10-20 17:56:05 +0700

    event

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import useDutyStore from "./store/useDutyStore";
 const App = () => {
   // JS
   const [adding, setAdding] = useState(false);
-
+  const [pending, setPending] = useState(null);
   const fetchAll = useDutyStore((state) => state.fetchAll);
 
   useEffect(() => {
@@ -18,14 +18,18 @@ const App = () => {
     fetchAll();
   }, []);
 
+  const onPick = (lat, lng) => {
+    setPending({ lat, lng });
+  };
+  console.log(pending);
+
   return (
     <div className="flex h-screen bg-gray-100">
       <PersonelList />
       <div className="flex flex-col flex-1">
         <Header adding={adding} setAdding={setAdding} />
-
         <div className="flex flex-1 overflow-hidden">
-          <MapView />
+          <MapView adding={adding} onPick={onPick} />
           <LocationsList />
         </div>
       </div>
--- src/components/map/MapView.jsx
+++ src/components/map/MapView.jsx
@@ -1,16 +1,26 @@
 // rafce
-import { MapContainer } from "react-leaflet";
+import { MapContainer, useMapEvents } from "react-leaflet";
 
 import "leaflet/dist/leaflet.css";
 import Layers from "./Layers";
 
-const MapView = () => {
+const ClickToAdd = ({ adding, onPick }) => {
+  useMapEvents({
+    click(e) {
+      if (adding) onPick(e.latlng.lat, e.latlng.lng);
+    },
+  });
+  return;
+};
+
+const MapView = ({ adding, onPick }) => {
   const center = [13, 100];
 
   return (
     <div className="flex-1">
       <MapContainer className="h-full" center={center} zoom={7}>
         <Layers />
+        <ClickToAdd adding={adding} onPick={onPick} />
       </MapContainer>
     </div>
   );
