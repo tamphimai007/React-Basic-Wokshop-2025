77386c99c0fbef3a354f0427e911aace0e9499cd
tamisan <phongphat.jap@gmail.com>
2025-10-21 21:18:42 +0700

    location

--- src/components/map/MapView.jsx
+++ src/components/map/MapView.jsx
@@ -1,8 +1,15 @@
 // rafce
-import { MapContainer, useMapEvents } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  Tooltip,
+  useMapEvents,
+} from "react-leaflet";
 
 import "leaflet/dist/leaflet.css";
 import Layers from "./Layers";
+import useDutyStore from "../../store/useDutyStore";
 
 const ClickToAdd = ({ adding, onPick }) => {
   useMapEvents({
@@ -14,6 +21,8 @@ const ClickToAdd = ({ adding, onPick }) => {
 };
 
 const MapView = ({ adding, onPick }) => {
+  const locations = useDutyStore((s) => s.locations);
+
   const center = [13, 100];
 
   return (
@@ -21,6 +30,20 @@ const MapView = ({ adding, onPick }) => {
       <MapContainer className="h-full" center={center} zoom={7}>
         <Layers />
         <ClickToAdd adding={adding} onPick={onPick} />
+
+        {locations.map((item) => {
+          return (
+            <Marker key={item.id} position={[item.lat, item.lng]}>
+              <Popup>
+                <div className="text-sm">{item.name}</div>
+                <div className="text-sm text-gray-500">
+                  {item.lat},{item.lng}
+                </div>
+              </Popup>
+              <Tooltip direction="center">{item.name}</Tooltip>
+            </Marker>
+          );
+        })}
       </MapContainer>
     </div>
   );
